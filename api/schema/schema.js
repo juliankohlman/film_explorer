80516eba5226e23614e96f3c8a9e39306dfda0a9
Julian Kohlman <juliankohlman@gmail.com>
2019-02-14 21:25:01 -0500

    Adds the movie genre type to the schema

--- api/schema/schema.js
+++ api/schema/schema.js
@@ -1,8 +1,11 @@
 import { makeSchema } from 'nexus';
-
+import { Genre } from '../schema/types/genres';
 // import queries in this file
 
 export const schema = makeSchema({
-	types: [],
-	outputs: {}
+	types: [Genre],
+	outputs: {
+		schema: __dirname + '/generated/schema.graphql',
+		typegen: __dirname + '/generated/typings.ts'
+	}
 });
