d63f021317b1f6b2fa3dbce047924965bf23f322
Julian Kohlman <juliankohlman@gmail.com>
2019-02-15 00:52:39 -0500

    Now playing request test successful in console

--- api/schema/queries/nowPlaying.js
+++ api/schema/queries/nowPlaying.js
@@ -1,5 +1,6 @@
 import { queryType } from 'nexus/dist';
-import { axios } from 'axios';
+import axios from 'axios';
+import { defaultFieldResolver } from 'graphql';
 
 export const NowPlaying = queryType({
 	name: 'NowPlaying',
@@ -9,25 +10,43 @@ export const NowPlaying = queryType({
 		t.string('poster_path');
 		t.string('title');
 		t.string('overview');
-		// ! Research how nexus handles resolvers in your schema
-		// t.resolveType(() =>
-		// 	axios
-		// 		.get(
-		// 			`https://api.themoviedb.org/3/movie/now_playing?api_key=${
-		// 				process.env.API
-		// 			}&language=en-US&page=1`
-		// 		)
-		// 		.then(res => {
-		// 			const movies = res.data.results;
-		// 			movies.map(movie => {
-		// 				movie.poster_path = `https://image.tmdb.org/t/p/w500${
-		// 					movie.poster_path
-		// 				}`;
-		// 				movie.overview;
-		// 			});
-
-		// 			return movies;
-		// 		})
-		// );
+		defaultFieldResolver: axios
+			.get(
+				`https://api.themoviedb.org/3/movie/now_playing?api_key=eb605f9e7a0e17bd244987219e32193a&language=en-US&page=1`
+			)
+			.then(res => {
+				const movies = res.data.results;
+				movies.map(movie => {
+					movie.poster_path = `https://image.tmdb.org/t/p/w500${
+						movie.poster_path
+					}`;
+					movie.overview;
+				});
+				console.log(movies[0]);
+				return movies;
+			})
+			.catch(err => {
+				console.log(err);
+			});
 	}
+	// ! Research how nexus handles resolvers in your schema
+	// t.resolveType(() =>
+	//   axios
+	//     .get(
+	//       `https://api.themoviedb.org/3/movie/now_playing?api_key=${
+	//       process.env.API
+	//       }&language=en-US&page=1`
+	//     )
+	//     .then(res => {
+	//       const movies = res.data.results;
+	//       movies.map(movie => {
+	//         movie.poster_path = `https://image.tmdb.org/t/p/w500${
+	//           movie.poster_path
+	//           }`;
+	//         movie.overview;
+	//       });
+
+	//       return movies;
+	//     })
+	// );
 });
--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 // const { GraphQLServer } = require('graphql-yoga');
 import { GraphQLServer } from 'graphql-yoga';
 import { schema } from './schema/schema';
+import dotenv from 'dotenv';
+dotenv.config();
 
 let port = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
 
-server.start(() => console.log(`Server is running on http://localhost:${port}`));
+server.start(() =>
+	console.log(`Server is running on http://localhost:${port}`)
+);
