77569c8e75ca4008f133dc096dc315aef56e1a64
Julian Kohlman <juliankohlman@gmail.com>
2019-02-22 12:23:36 -0500

    Updates schema to allow null values on certain fields

--- api/schema/schema.js
+++ api/schema/schema.js
@@ -7,25 +7,25 @@ export const typeDefs = `
 
   type NowPlaying {
     id: ID!
-		poster_path: String!
+		poster_path: String
 		title: String!
 		overview: String!
   }
 
   type FilmDetails {
-    backdrop_path: String!
+    backdrop_path: String
     budget: Int!
     genres: String!
     id: ID!
     overview: String!
-    popularity: Int!
-    poster_path: String!
+    popularity: Float!
+    poster_path: String
     production_companies: String!
     release_date: String!
     revenue: Int!
-    runtime: String!
+    runtime: String
     status: String!
-    tagline: String!
+    tagline: String
     title: String!
   }
 `;
