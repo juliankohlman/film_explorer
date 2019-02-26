336fa0ef159dc057ecef27ce2280b6defc3e5aee
Julian Kohlman <juliankohlman@gmail.com>
2019-02-26 10:41:41 -0500

    Tests dynamic query string builder with different combinations of params

--- api/schema/tmdb-connector.js
+++ api/schema/tmdb-connector.js
@@ -17,7 +17,6 @@ export const nowPlaying = (_, { page }) =>
 			`https://api.themoviedb.org/3/movie/now_playing?api_key=${API}&language=en-US&page=${page}`
 		)
 		.then(res => {
-			//! Add pagination functionality to typeDef
 			console.log('# of pages:', res.data.total_pages);
 			console.log('# of movies:', res.data.total_results);
 
@@ -97,24 +96,8 @@ export const genreQuery = (
 		with_runtime_lte
 	}
 ) => {
-	//TODO Add logic to dynamically build query string based on arguments
-	// [https://api.themoviedb.org/3/discover/movie?api_key=${API}&language=en-US]
-	// [&sort_by section]
-	// [&certification ]
-	//! [&include_adult=false]
-	//! [&include_video=false]
-	// [&page]
-	// [&primary_release_date.gte] year-month-day (after)
-	// [&primary_release_date.lte] year-month-day (before)
-	// [&year]
-	// [&with_runtime.gte]
-	// [&with_runtime.lte]
-	// ? iterate over query sections
-	// ? IF a query arg exists...!== undefined
-	// * then insert that section into query string
-	// * IF query arg is empty move onto the next
-	//  HARD CODE GENRE FROM client-side label somehow
-	console.log(sort_by);
+	//TODO Add logic to dynamically build query string based on arguments HARD CODE GENRE FROM client-side label somehow
+	// console.log(sort_by);
 	// https://api.themoviedb.org/3/discover/movie?api_key=${API}&language=en-US
 	let queryArguments = [
 		sort_by,
@@ -136,7 +119,8 @@ export const genreQuery = (
 		`&sort_by=${sort_by}`,
 		`&certification_country=${certification_country}`,
 		`&certification=${certification}`,
-		'&include_adult=false&include_video=false',
+		`&include_adult=${include_adult}`,
+		`&include_video=${include_video}`,
 		`&page=${page}`,
 		`&primary_release_date.gte=${primary_release_date_gte}`,
 		`&primary_release_date.lte=${primary_release_date_lte}`,
@@ -147,22 +131,39 @@ export const genreQuery = (
 	];
 
 	let query = `https://api.themoviedb.org/3/discover/movie?api_key=${API}&language=en-US`;
-	return axios
-		.get(
-			`https://api.themoviedb.org/3/discover/movie?api_key=${API}&language=en-US&include_adult=false&include_video=false&page=${page}&with_genres=${genreID}`
-		)
-		.then(res => {
-			console.log('# of pages:', res.data.total_pages);
-			console.log('# of movies:', res.data.total_results);
+	// ratings check b/f looping
+	if (queryArguments[2]) {
+		queryArguments[1] = 'US';
+		queryPropValues[1] = `&certification_country=US`;
+	}
+	console.log('certification country', queryArguments[1]);
 
-			const genreFilms = res.data.results;
-			genreFilms.map(film => {
-				film.poster_path = `https://image.tmdb.org/t/p/w500${film.poster_path}`;
-				film.overview;
-			});
-			return genreFilms;
-		})
-		.catch(e => res.json('error', e));
+	queryArguments.forEach((arg, idx) =>
+		arg !== undefined ? (query += queryPropValues[idx]) : null
+	);
+	console.log(query);
+
+	return (
+		axios
+			// .get(
+			// 	`https://api.themoviedb.org/3/discover/movie?api_key=${API}&language=en-US&include_adult=false&include_video=false&page=${page}&with_genres=${genreID}`
+			// )
+			.get(query)
+			.then(res => {
+				console.log('# of pages:', res.data.total_pages);
+				console.log('# of movies:', res.data.total_results);
+
+				const genreFilms = res.data.results;
+				genreFilms.map(film => {
+					film.poster_path = `https://image.tmdb.org/t/p/w500${
+						film.poster_path
+					}`;
+					film.overview;
+				});
+				return genreFilms;
+			})
+			.catch(e => res.json('error', e))
+	);
 };
 // sort by options
 // [popularity.asc, popularity.desc, release_date.asc, release_date.desc, revenue.asc, revenue.desc, primary_release_date.asc, primary_release_date.desc, original_title.asc, original_title.desc, vote_average.asc, vote_average.desc, vote_count.asc, vote_count.desc]
