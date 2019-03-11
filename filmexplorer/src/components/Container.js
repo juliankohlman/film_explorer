2133434ef4d65b03803f7d9ada1b02b2d03160a1
Julian Kohlman <juliankohlman@gmail.com>
2019-03-10 22:30:47 -0400

    Adds composable header/nav components

--- filmexplorer/src/components/Container.js
+++ filmexplorer/src/components/Container.js
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
+import LandingNav from '../pages/landing/LandingNav';
 
 const logo = require('../images/logo.png');
-// import logo from './logo.png';
 
-export default class Container extends Component {
-	render() {
-		return (
-			<div>
-				<header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
-					<a class="v-mid mid-gray link dim w-25" href="#" title="Home">
-						<img src={logo} class="dib w3 h3 br-100" alt="Site Name" />
-					</a>
-					<nav class="f6 fw6 ttu tracked fr">
-						{/* original link styling `link dim white dib mr3` */}
-						<a class="link dim white dib mr3" href="#" title="Home">
-							Home
-						</a>
-						<a class="link dim white dib mr3" href="#" title="About">
-							About
-						</a>
-						<a class="link dim white dib mr3" href="#" title="Store">
-							Store
-						</a>
-						<a class="link dim white dib" href="#" title="Contact">
-							Contact
-						</a>
-					</nav>
-				</header>
-			</div>
-		);
-	}
+export default function Container({ navigation }) {
+	return (
+		<div>
+			<header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+				<a class="dtc v-mid mid-gray link dim w-25" href="#" title="Home">
+					<img src={logo} class="dib w3 h3 br-100" alt="Site Name" />
+				</a>
+				<LandingNav />
+			</header>
+		</div>
+	);
 }
---
+++ filmexplorer/src/pages/landing/LandingNav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function LandingNav() {
+	return (
+		<nav class="dtc v-mid w-75">
+			{/* original link styling `link dim white dib mr3` */}
+			<a class="link dim white dib mh3" href="#" title="Home">
+				Home
+			</a>
+			<a class="link dim white dib mh3" href="#" title="About">
+				About
+			</a>
+			<a class="link dim white dib mh3" href="#" title="Store">
+				Store
+			</a>
+			<a class="link dim white dib" href="#" title="Contact">
+				Contact
+			</a>
+		</nav>
+	);
+}
