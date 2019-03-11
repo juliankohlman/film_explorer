2119ed1c500cdf1f9460a4f37ee11c56cdec7254
Julian Kohlman <juliankohlman@gmail.com>
2019-03-10 22:33:05 -0400

    Refactors container to class component

--- filmexplorer/src/components/Container.js
+++ filmexplorer/src/components/Container.js
@@ -3,15 +3,18 @@ import LandingNav from '../pages/landing/LandingNav';
 
 const logo = require('../images/logo.png');
 
-export default function Container({ navigation }) {
-	return (
-		<div>
-			<header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
-				<a class="dtc v-mid mid-gray link dim w-25" href="#" title="Home">
-					<img src={logo} class="dib w3 h3 br-100" alt="Site Name" />
-				</a>
-				<LandingNav />
-			</header>
-		</div>
-	);
+export default class Container extends Component {
+	render() {
+		return (
+			<div>
+				<header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+					<a class="dtc v-mid mid-gray link dim w-25" href="#" title="Home">
+						<img src={logo} class="dib w3 h3 br-100" alt="Site Name" />
+					</a>
+					{/* Custom Navigation Component */}
+					<LandingNav />
+				</header>
+			</div>
+		);
+	}
 }
