99efe04c9042b04e9f9ce721201e39f17be943d2
Julian Kohlman <juliankohlman@gmail.com>
2019-03-19 22:43:34 -0400

    Adds notes to backdrop component code for next steps

---
+++ filmexplorer/src/components/FilmBackdrop.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+export default class FilmBackdrop extends PureComponent {
+  render() {
+    return (
+      <div>
+        //todo: add banner of some sort from film data.
+        //todo: will conditionally render a placeholder banner if the film has no backdrop data.
+      </div>
+    )
+  }
+}
