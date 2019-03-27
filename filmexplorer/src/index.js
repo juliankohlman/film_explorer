8cc3d8f8d5d4b781c6d0c4b4121a3fdb39753a2e
Julian Kohlman <juliankohlman@gmail.com>
2019-03-26 21:02:39 -0400

    Updates Router on index.js

--- filmexplorer/src/index.js
+++ filmexplorer/src/index.js
@@ -16,7 +16,6 @@ ReactDOM.render(
 			<Switch>
 				<Route exact path="/" component={Landing} />
 				<Route exact path="/genre/:key" component={Genre} />
-
 				<Route exact path="/detail/:id" component={Detail} />
 			</Switch>
 		</ApolloProvider>
