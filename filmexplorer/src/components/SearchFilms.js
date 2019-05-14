0c33789c8a812c8baa7e9dea6c10afd2fb5e2284
Julian Kohlman <juliankohlman@gmail.com>
2019-05-14 13:49:07 -0400

    Updating search film results to add dynamic pagination

--- filmexplorer/src/components/SearchFilms.js
+++ filmexplorer/src/components/SearchFilms.js
@@ -2,42 +2,99 @@ import React, { Component } from 'react';
 import { Query, graphql } from 'react-apollo';
 import { Link } from 'react-router-dom';
 import { SEARCH_FILM } from '../queries/searchFilm';
+import FilmPage from './FilmPage';
 
 //TODO know difference between withApollo vs graphql()
 
 class SearchFilms extends Component {
 	// Todo add pagination, which will get refactored to be more D.R.Y later
+	constructor(props) {
+		super(props);
+		this.state = {
+			page: 1
+		};
+	}
 	render() {
-		const FilmSearchPosters = ({ query }) => (
-			<Query query={SEARCH_FILM} variables={{ queryString: this.props.query }}>
-				{({ loading, error, data }) => {
+		const FilmSearchPosters = () => (
+			<Query
+				query={SEARCH_FILM}
+				variables={{ queryString: this.props.query, page: this.state.page }}
+			>
+				{({ loading, error, data, fetchMore }) => {
 					if (loading) return 'Loading...';
 					if (error) return `Error! ${error.message}`;
 					console.log(data.searchFilm);
 
 					return (
-						<article style={{ paddingTop: '342px' }}>
-							{data.searchFilm.map(film => (
-								<Link
-									to={`/detail/${film.id}`}
-									className="fl w-50 w-25-l link overflow-hidden"
-									key={film.id}
-								>
-									<div
-										role="img"
-										aria-label={film.title}
-										className="grow aspect-ratio--4x6 "
-										style={{
-											background: `url(${
-												film.poster_path
-											}) no-repeat center center`,
-											backgroundSize: 'cover'
-										}}
-									/>
-								</Link>
-							))}
-						</article>
+						<div>
+							<FilmPage
+								films={data.searchFilm || []}
+								nextPage={() =>
+									fetchMore({
+										variables: {
+											page: this.setState(state => {
+												return {
+													page: (state.page += 1
+														? (state.page += 1)
+														: state.page)
+												};
+											})
+										},
+										updateQuery: (prevPage, { fetchMoreResult }) => {
+											if (!fetchMoreResult) return prevPage;
+											return {
+												searchFilm: [...fetchMoreResult.searchFilm]
+											};
+										}
+									})
+								}
+								// Todo debug page boundary error.
+								lastPage={() =>
+									fetchMore({
+										variables: {
+											page: this.setState(state => {
+												return state.page === 1
+													? { page: state.page }
+													: {
+															page: (state.page -= 1)
+													  };
+											})
+										},
+										updateQuery: (prevPage, { fetchMoreResult }) => {
+											if (!fetchMoreResult) return prevPage;
+											return {
+												searchFilm: [...fetchMoreResult.searchFilm]
+											};
+										}
+									})
+								}
+							/>
+						</div>
 					);
+
+					// return (
+					// 	<article style={{ paddingTop: '342px' }}>
+					// 		{data.searchFilm.map(film => (
+					// 			<Link
+					// 				to={`/detail/${film.id}`}
+					// 				className="fl w-50 w-25-l link overflow-hidden"
+					// 				key={film.id}
+					// 			>
+					// 				<div
+					// 					role="img"
+					// 					aria-label={film.title}
+					// 					className="grow aspect-ratio--4x6 "
+					// 					style={{
+					// 						background: `url(${
+					// 							film.poster_path
+					// 						}) no-repeat center center`,
+					// 						backgroundSize: 'cover'
+					// 					}}
+					// 				/>
+					// 			</Link>
+					// 		))}
+					// 	</article>
+					// );
 				}}
 			</Query>
 		);
