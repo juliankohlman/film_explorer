a543055e6f8fdef5f108ea42160bdb6ee2d96b75
Julian Kohlman <juliankohlman@gmail.com>
2019-05-28 13:07:41 -0400

    Adds documentation to helpers functions

--- api/schema/helpers.js
+++ api/schema/helpers.js
@@ -2,6 +2,12 @@
 // Use original image size if poster exists
 // If poster property is null use a placeholder
 //Todo add more dynamic placeholder posters using randomly generated colors for background: May need to create your own placeholders to inject
+/**
+ *
+ * @param {string} posterPath string representing a poster images url
+ * @param {film} film object representing a single film
+ * @returns {String} a dynamic poster image url or a placeholder url
+ */
 export const posterImageCheck = (posterPath, film) => {
 	if (!posterPath) {
 		let posterText = film.title.split(' ').join('+');
@@ -12,6 +18,12 @@ export const posterImageCheck = (posterPath, film) => {
 	return posterPath;
 };
 
+/**
+ *
+ * @param {string} backdropPath string
+ * @param {film} film object representing a single film
+ * @returns {String} a dynamic backdrop image url or a placeholder url
+ */
 export const backdropCheck = (backdropPath, film) => {
 	if (!backdropPath) {
 		let backdropText = film.title.split(' ').join('+');
