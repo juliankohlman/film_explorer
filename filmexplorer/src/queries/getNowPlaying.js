201af0b355f79ef8016fc3b8c7783a2622a7f4b0
Julian Kohlman <juliankohlman@gmail.com>
2019-06-18 15:45:14 -0400

    Removes old modules dir and adds node-sass

--- filmexplorer/src/queries/getNowPlaying.js
+++ filmexplorer/src/queries/getNowPlaying.js
@@ -1,5 +1,5 @@
 import { gql } from 'apollo-boost';
-
+//Todo be sure to tune queries and only grab the fields you'll need. Nowplaying only really needs ids' and poster paths for example
 export const GET_NOW_PLAYING = gql`
 	query($page: Int = 1) {
 		getNowPlaying(page: $page) {
