b97c2caf9c89d81ba15cee69ab21aed9fecf6a26
Julian Kohlman <juliankohlman@gmail.com>
2019-07-19 15:49:01 -0400

    Debugging spacing issue on metaData section of detail component (tablet view)

--- filmexplorer/src/components/UI/FilmMetaData.js
+++ filmexplorer/src/components/UI/FilmMetaData.js
@@ -14,26 +14,31 @@ const FilmMetaData = props => {
 	return (
 		<ul className="metaData">
 			<li>
-				<label className="metaLabel">Revenue:</label> {revenue}
+				<label className="metaLabel">Revenue: </label>
+				{revenue}
 			</li>
 			<li>
-				<label className="metaLabel">Budget:</label> {budget}
+				<label className="metaLabel">Budget: </label>
+				{budget}
 			</li>
 			<li>
-				<label className="metaLabel">Runtime:</label> {runtime}
+				<label className="metaLabel">Runtime: </label>
+				{runtime}
 			</li>
 			<li>
-				<label className="metaLabel">Status:</label> {status}
-			</li>
-			{/* Look into more readable date format */}
-			<li>
-				<label className="metaLabel">Release Date:</label> {release_date}
+				<label className="metaLabel">Status: </label>
+				{status}
 			</li>
 			<li>
-				<label className="metaLabel">Similar:</label> {similar || 'Not found'}
+				<label className="metaLabel">Release Date: </label>
+				{release_date}
 			</li>
 			<li>
-				<label className="metaLabel">Recommendations:</label>{' '}
+				<label className="metaLabel">Similar: </label>
+				{similar || 'Not found'}
+			</li>
+			<li>
+				<label className="metaLabel">Recommendations: </label>
 				{recommendations || 'Not found'}
 			</li>
 		</ul>
