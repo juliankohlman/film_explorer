1a334935b15e7b7665acaef3923bd3b0393089cb
Julian Kohlman <juliankohlman@gmail.com>
2019-07-25 18:03:40 -0400

    Reverted back to prior folder structure

--- src/components/UI/Container.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Container = props => <div style={props.style}>{props.children}</div>;
-
-// function Container(props) {
-// 	return <div>{props.children}</div>;
-// }
-
-export default Container;
--- webpack.functions.js
+++
@@ -1,5 +0,0 @@
-// To run use: netlify-lambda serve --config ./webpack.functions.js
-
-module.exports = {
-	plugins: [new Dotenv()]
-};
