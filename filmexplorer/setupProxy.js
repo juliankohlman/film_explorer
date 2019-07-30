a0c0e1256edd20c46ae416c9791067b26ed42a4f
Julian Kohlman <juliankohlman@gmail.com>
2019-07-29 20:12:30 -0400

    Changes proxy target to deployed url

--- filmexplorer/setupProxy.js
+++ filmexplorer/setupProxy.js
@@ -5,7 +5,8 @@ module.exports = function(app) {
 	app.use(
 		proxy('/.netlify/functions/', {
 			// target: 'http://localhost:3000/',
-			target: 'http://localhost:9000/',
+			// target: 'http://localhost:9000/',
+			target: 'https://film-explorer.netlify.com/',
 			pathRewrite: {
 				'^/\\.netlify/functions': ''
 			}
