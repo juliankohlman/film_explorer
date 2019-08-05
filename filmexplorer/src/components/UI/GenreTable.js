14bd76f7456c0894c4097a4d21e435b7bcf5dec2
Julian Kohlman <juliankohlman@gmail.com>
2019-08-04 22:21:18 -0400

    uncomments previous genreTable code

--- filmexplorer/src/components/UI/GenreTable.js
+++ filmexplorer/src/components/UI/GenreTable.js
@@ -7,52 +7,75 @@ import { genreIDs, genres } from '../../utils/genres';
 import { GET_GENRE } from '../../queries/getGenre';
 import { ApolloConsumer } from 'react-apollo';
 
-class GenreTable extends Component {
-	// TODO add query component HERE!!!!
-	state = {
-		genre: null,
-		page: 1
-	};
-
-	onGenreFetched = genre => this.setState(() => ({ genre }));
-	render() {
-		return (
-			<ApolloConsumer>
-				{client => (
-					<section className="genreTable">
-						{Object.keys(genreIDs).map((genre, idx) => (
-							<Link
-								to={`/genre/${genre}`}
-								key={genre}
-								style={{
-									margin: '25px',
-									background: '#fff',
-									borderRadius: '10px',
-									textDecoration: 'none'
-									// color: 'red'
-								}}
-								// ref={genreIDs[genre]['id']}
-								onClick={async () => {
-									const { data } = await client.query({
-										query: GET_GENRE,
-										variables: {
-											genreID: genreIDs[genre].id,
-											page: this.state.page
-										}
-									});
-									this.onGenreFetched(data.getGenre);
-								}}
-								//Todo ADD ONCLICK HERE!!!!
-							>
-								<GenreCard key={genreIDs.genre} genre={genres[idx]} />
-							</Link>
-						))}
-					</section>
-				)}
-			</ApolloConsumer>
-		);
-	}
+// import React from 'react'
+export default function GenreTable() {
+	return (
+		<section className="genreTable">
+			{Object.keys(genreIDs).map((genre, idx) => (
+				<Link
+					to={`/genre/${genre}`}
+					key={genre}
+					style={{
+						margin: '25px',
+						background: '#fff',
+						borderRadius: '10px',
+						textDecoration: 'none'
+						// color: 'red'
+					}}
+				>
+					<GenreCard key={genreIDs.genre} genre={genres[idx]} />
+				</Link>
+			))}
+		</section>
+	);
 }
+
+// class GenreTable extends Component {
+// 	// TODO add query component HERE!!!!
+// 	state = {
+// 		genre: null,
+// 		page: 1
+// 	};
+
+// 	onGenreFetched = genre => this.setState(() => ({ genre }));
+// 	render() {
+// 		return (
+// 			<ApolloConsumer>
+// 				{client => (
+// 					<section className="genreTable">
+// 						{Object.keys(genreIDs).map((genre, idx) => (
+// 							<Link
+// 								to={`/genre/${genre}`}
+// 								key={genre}
+// 								style={{
+// 									margin: '25px',
+// 									background: '#fff',
+// 									borderRadius: '10px',
+// 									textDecoration: 'none'
+// 									// color: 'red'
+// 								}}
+// 								// ref={genreIDs[genre]['id']}
+// 								onClick={async () => {
+// 									const { data } = await client.query({
+// 										query: GET_GENRE,
+// 										variables: {
+// 											genreID: genreIDs[genre].id,
+// 											page: this.state.page
+// 										}
+// 									});
+// 									this.onGenreFetched(data.getGenre);
+// 								}}
+// 								//Todo ADD ONCLICK HERE!!!!
+// 							>
+// 								<GenreCard key={genreIDs.genre} genre={genres[idx]} />
+// 							</Link>
+// 						))}
+// 					</section>
+// 				)}
+// 			</ApolloConsumer>
+// 		);
+// 	}
+// }
 
 /**
  * 	<section className="genreTable">
@@ -74,4 +97,4 @@ class GenreTable extends Component {
 					</section>
  */
 
-export default graphql(GET_GENRE)(GenreTable);
+// export default graphql(GET_GENRE)(GenreTable);
